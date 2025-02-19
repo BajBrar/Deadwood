1d3e44453ef95bb305ef8790e6805c9be76a3790
Baj Brar <your-bajbrar150@icloud.com>
2025-02-19 00:10:53 -0800

    MVC additions and adding methods

--- GameController.java
+++ GameController.java
@@ -1,3 +1,43 @@
+//This class is the controller for the game. It will handle the game logic for MVC pattern.
+
+import java.util.List;
+
 public class GameController {
-    
+    private List<Player> players;
+    private Board board;
+    private Dice dice;
+    private int currentPlayerIndex;
+
+    public GameController(List<Player> players, Board board, Dice dice) {
+        this.players = players;
+        this.board = board;
+        this.dice = dice;
+        //need this var to keep track of what players turn it is
+        this.currentPlayerIndex = 0; 
+    }
+    //we might need more methods to handle the game logic
+
+    public Player getCurrentPlayer() {
+        return players.get(currentPlayerIndex);
+    }
+
+    public void movePlayer(String newPosition) {
+       
+    }
+
+    public void playerRehearse() {
+       
+    }
+
+    public void playerAct() {
+       
+    }
+
+    public void upgradePlayer(int newRank, int costDollars, int costCredits) {
+       
+    }
+
+    public void endTurn() {
+        
+    }
 }
---
+++ GameView.java
@@ -0,0 +1,20 @@
+//Anything that needs to be printed needs to go in this class to make it easier for interface implementation later on and its easier for the controller to call the view to print things.
+
+//The example thats in assignment 2 description all of those system.out.println's should be in this class.
+import java.util.Scanner;
+
+public class GameView {
+    private Scanner scanner;
+
+    public GameView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    //add all the display methods here
+
+
+    //close the scanner 
+    public void closeScanner() {
+        scanner.close();
+    }
+}
