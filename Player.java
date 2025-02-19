d06c79465c27f24dc31ff57f98b1f64652a0ccb6
Nalpind <156188078+Nalpind@users.noreply.github.com>
2025-02-18 22:30:08 -0800

    Beginning Player class

--- Player.java
+++ Player.java
@@ -1,3 +1,20 @@
 public class Player{
+  public int playerNum;
+  public int rank;
+  public int dollars;
+  public int credits;
+  public int practiceChips;
+  public String status;
+  //Possibly be changed for a different class such as space.
+  public String startPos;
+  public String curPos;
+
+
+//Set up the players based on the total number of players playing the game.
+  public Player(int pNum, int pCount) {
+    this.playerNum = pNum;
+  }
+
+
   
 }
