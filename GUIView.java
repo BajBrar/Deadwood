5c4993777025135fadc2c2ffac2c6f4c48dceb1d
Grey Pendras <gpendras@gmail.com>
2025-03-07 11:46:41 -0800

    Added GUI files and boardlistener skeleton code

---
+++ GUIView.java
@@ -0,0 +1,33 @@
+import java.util.Set;
+import javax.swing.*;
+
+class GUIView implements View {
+    JFrame frame;
+    JLabel l1;
+    JLabel l2;
+    JLabel l3;
+    
+    public GUIView() {
+        frame = new JFrame("Deadwood View");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+    
+    
+    
+    
+}
